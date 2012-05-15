313d97fbe90b651807ec63bc518168209003de35
Darren VanBuren <onekopaka@theoks.net>
2012-05-14 19:43:50 -0700

    Clean up config file, that <center> was bad.

--- configurationfile.php
+++ configurationfile.php
@@ -1,28 +1,30 @@
 <?php
+/*
+ * UnBlockedPirateBay-Freedom config file
+ *
+ * Project home: https://github.com/onekopaka/UnBlockedPirateBay-Freedom/
+ */
+
 //Want some visitors on your proxy? Submit your link here: http://about.piratereverse.info/proxy/list.html
 
-
-//More stuff will be added here in future versions. Check http://unblockedpiratebay.com/external/ occasionally for updates.
-//You are currently using v1.0
-
-//The stuff in between the title tags
+// The stuff in between the title tags
 $pagetitle = "Download music, movies, games, software! The Mirror Bay";
 
-//Anything you add here (including html) will be added into the footer
-$footer = "<center>Powered by <a href=\"https://github.com/onekopaka/UnBlockedPirateBay-Freedom\">UnBlockedPirateBay-Freedom</a>.</center>";
+// Anything you add here (including html) will be added into the footer
+$footer = "Powered by <a href=\"https://github.com/onekopaka/UnBlockedPirateBay-Freedom\">UnBlockedPirateBay-Freedom</a>.";
 
-//Images on bayimg.com - Do you want to proxy them or let the user load them directly? 
-//This feature is mainly future proofing, incase bayimg IP's are included in tpb ban.
-//1 = yes, 0 = no
+// Images on bayimg.com - Do you want to proxy them or let the user load them directly? 
+// This feature is mainly future proofing, incase bayimg IP's are included in tpb ban.
+// 1 = yes, 0 = no
 $proxy_bayimg = "0";
 
-//Advert code on top of pages (remember when using " in your ad code, eg <a href="http://
-//you have to backslash them eg a href=\"http://
-//Shows on all pages except homepage, edit this yourself with ease
+// Advert code on top of pages (remember when using " in your ad code, eg <a href="http://
+// you have to backslash them eg a href=\"http://
+// Shows on all pages except homepage, edit this yourself with ease
 $codetop = "<center>Download responsibly!</center>";
 
-//You also may need to edit cookies.txt and empty the contents every now and then
-//Plans for a future version involve doing it automatically daily..
+// You also may need to edit cookies.txt and empty the contents every now and then
+// Plans for a future version involve doing it automatically daily..
 
-//To edit the logo on the homepage edit static/img/homelogo.jpg
+// To edit the logo on the homepage edit static/img/homelogo.jpg
 ?>
